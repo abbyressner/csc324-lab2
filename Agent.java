21668a315cbf05b512526a3f2d98e20ddc63c702
Yash Malik Lenovo Portable <yash.malik0606@gmail.com>
2025-09-15 15:27:49 -0500

    Agent.java

--- Agent.java
+++ Agent.java
@@ -1,7 +1,21 @@
 public class Agent{
 
-  int[] initialState;
+  int[] state = {this.x, this.y};
+  int x;
+  int y;
+
   public static void main(String[] args){
 
+  }
+
+  void initAgent(){
+    // Pick random x coord
+    // Pick random y coord
+    // If (x,y) is empty then place agent there to start
+  }
+
+  void act(){
+    // {left, right, up, down}
+    // this.state = nextState
   }
 }
